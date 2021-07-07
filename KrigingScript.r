c1ad9c30fe285de3c46429f7a32120ab65dc8ade
atsironis <58917711+atsironis@users.noreply.github.com>
2021-07-07 03:56:47 +0300

    Update KrigingScript.r

--- KrigingScript.r
+++ KrigingScript.r
@@ -11,13 +11,13 @@ pro79<-read.table('C:/Users/atsironis/Documents/University/Master/Geography/Fina
 loc<- pro79 [,2:1]
 #αποθήκευση της 3ης στήλης στη μεταβλητή amp
 amp<- pro79 [,3]
-
+# απεικόνιση
 as.image(amp,loc,na.rm=T)->look
-
+# σχεδιασμός χάρτη με καθορισμό ορίων και εισαγωγή τίτλου
 image.plot(look, xlab="lon", ylab="lat", xlim=c(10,40), ylim=c(40,70), zlim=c(5,30), main="1979-2021 Humidity in Estonia ")
-
+#εισαγωγή γεωγραφικού περιγράμματος
 addland()
-
+# εισαγωγή πλέγματος
 grid()
 
 Krig(loc,Y=amp)->out
